c4fcd987b297f591d14de55beb9b7e645e1f067f
rode-alb <rodrigosantoslima78@gmail.com>
2022-07-29 00:50:45 +0100

    C08

---
+++ C08/ex02/ft_abs.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_abs.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rode-alb <rode-alb@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/29 00:08:28 by rode-alb          #+#    #+#             */
+/*   Updated: 2022/07/29 00:08:28 by rode-alb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_ABS_H
+# define FT_ABS_H
+
+#define ABS(Value) (Value < 0) ? (Value * -1) : (Value)
+
+//this means: if Value < 0 then do Value*-1. if value not < 0 do Value.
+// with this I just created a macro
+
+#endif
---
+++ C08/ex03/ft_point.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rode-alb <rode-alb@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/29 00:31:42 by rode-alb          #+#    #+#             */
+/*   Updated: 2022/07/29 00:31:42 by rode-alb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct s_point
+{
+        int x;
+        int y;
+}   t_point;
+
+#endif
+
+//structs are a way to group several related variables into one place.
+//Each variable in the structure is known as a member of the structure.
